913e7cdb3dd7777b57d3818f327dbc608b0bd882
neiltruong <truongtu678@gmail.com>
2025-11-07 21:45:52 +0700

    update api user

--- fe-user/src/services/api.js
+++ fe-user/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json'
   }
@@ -19,9 +19,7 @@ api.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 // Add response interceptor
