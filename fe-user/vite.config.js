7b9e6fad12f13ee7d59f46a44678f35a2c6f7b04
neiltruong <truongtu678@gmail.com>
2025-11-14 00:34:13 +0700

    Update vite config

--- fe-user/vite.config.js
+++ fe-user/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Giữ nguyên các file tĩnh trong public, không bị Vite inject JS
+      input: {
+        main: 'index.html',
+      },
+    },
+  },
+  publicDir: 'public', // đảm bảo Vite copy toàn bộ file trong public
 })
+
